399f869c60a620b90458e5e8d6a5acadc14aa348
arispriatna10 <137760740+arispriatna10@users.noreply.github.com>
2024-08-21 12:33:55 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.write(""""
+# Aplikasi Cek BBM
+Ini adalah aplikasi mengecek kebutuhan BBM Perjalanan Dinas
+""")
+
+original_list = ["Pilih Kabupaten/Kota","Kota Bandung","DKI Jakarta", "Kab Bandung Barat"]
+
+
+asal = st.selectbox("Masukan Tempat Asal",original_list)
+tujuan = st.selectbox("Masukan Tempat Tujuan",original_list)
+
+if asal == "Kota Bandung" and tujuan == "DKI Jakarta":
+   st.write ("Jumlah Maksimalnya adalah", 15)
+
+elif asal == "Kota Bandung" and tujuan == "Kab Bandung Barat":
+     st.write ("Jumlah Maksimalnya adalah", 20)
+
+
+   
