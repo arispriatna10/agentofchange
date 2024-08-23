5e9feb379db1b38593e3fba443b20cec6766e7c4
agent of change <137760740+arispriatna10@users.noreply.github.com>
2024-08-23 17:09:14 +0700

    mainn.py

--- mainn.py
+++ mainn.py
@@ -37,7 +37,7 @@ original_list = ["Pilih Kabupaten/Kota",
                  "DKI Jakarta"]
 
 
-asal = st.text.input("Masukan Tempat Asal","Bandung")
+asal = st.text_input("Masukan Tempat Asal","Bandung")
 tujuan = st.selectbox("Masukan Tempat Tujuan",original_list)
 
 if asal == "Kota Bandung" and tujuan == "Kab. Bandung":
