a048357b1ff70df3686603408029b8e81bcf8741
arispriatna10 <137760740+arispriatna10@users.noreply.github.com>
2024-08-23 10:22:51 +0700

    main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ tujuan = st.selectbox("Masukan Tempat Tujuan",original_list)
 
 if asal == "Kota Bandung" and tujuan == "DKI Jakarta":
    st.write ("Jumlah Maksimalnya adalah", 15)
+   st.write ("Jumalh Minimalnya adalah", 15)
 
 elif asal == "Kota Bandung" and tujuan == "Kab Bandung Barat":
      st.write ("Jumlah Maksimalnya adalah", 20)
