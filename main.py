90f30f0c6f5db854d8539de11c9086ce4420b888
arispriatna10 <137760740+arispriatna10@users.noreply.github.com>
2024-08-23 10:30:56 +0700

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ st.write(""""
 Ini adalah aplikasi mengecek kebutuhan BBM Perjalanan Dinas
 """)
 
-original_list = ["Pilih Kabupaten/Kota","Belanja Langsung","DKI Jakarta", "Kab Bandung Barat"]
+original_list = ["Pilih Jenis Belanja",
+                 "Belanja Perjalanan Dinas",
+                 "DKI Jakarta", 
+                 "Kab Bandung Barat"]
 
 
 asal = st.selectbox("Masukan Tempat Asal",original_list)
@@ -15,8 +18,15 @@ if asal == "Belanja Langsung":
    st.write ("Dokumen Kelengkapan:")
    st.write ("1. Surat Permintaan Pembayaran", 15)
 
-elif asal == "Kota Bandung" and tujuan == "Kab Bandung Barat":
-     st.write ("Jumlah Maksimalnya adalah", 20)
+elif asal == "Belanja Perjalanan Dinas":
+   st.write ("Dokumen Kelengkapan:")
+   st.write ("1. Rampung Rincian Belanja Perjalanan Dinas")
+   st.write ("2. Daftar Nominatif")
+   st.write ("3. Surat Perintah")
+   st.write ("4. Visum Surat Perjalanan Dinas")
+   st.write ("5. Laporan dan Dokumentasi Perjalanan Dinas")
+   st.write ("6. Tanda Bukti BBM, Tol, Tiket Pesawat, dan lain-lain")
+
 
 
    
