0883f7efc0be641006c5d514bca39dc9000e9096
agent of change <137760740+arispriatna10@users.noreply.github.com>
2024-09-03 11:13:57 +0700

    main1.py

--- main1.py
+++ main1.py
@@ -7,21 +7,20 @@ Ini adalah aplikasi untuk mengecek Gaji Pokok Berdasarkan Masa Kerja dan Golonga
 
 
 golongan_gendut = ["Pilih Golongan",
-                 "Golongan IIa",
-                 "Golongan IIb"]
+                 "Golongan Ia",
+                 "Golongan Ib"]
 masa_kerja = ["Pilih Masa Kerja", 
-              1, 
-              2]
+              1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
                  
 
 golongan_gendutt = st.selectbox("Masukan Golongan",golongan_gendut)
 masa_kerjaa = st.selectbox("Masukan Masa Kerja (Dalam Tahun)",masa_kerja)
 
-if golongan_gendutt == "Golongan IIa" and masa_kerjaa == 1:
+if golongan_gendutt == "Golongan Ia" and masa_kerjaa == 0:
   st.write ("Gaji Pokoknya adalah 2.300.000")
 
-elif golongan_gendutt == "Golongan IIb" and masa_kerjaa == "1 Tahun":
-  st.write ("Gaji Pokoknya adalah 2.300.000")
+if golongan_gendutt == "Golongan Ia" and masa_kerjaa <2:
+  st.write ("Gaji Pokoknya adalah 2.300.500")
 
 
 
