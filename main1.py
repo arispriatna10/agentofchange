d8811a0d265f757465bd12c4ce708f796f930735
agent of change <137760740+arispriatna10@users.noreply.github.com>
2024-09-09 10:54:42 +0700

    main1.py

--- main1.py
+++ main1.py
@@ -5,15 +5,15 @@ st.write("""
 Ini adalah aplikasi untuk mengecek Gaji Pokok Berdasarkan Masa Kerja dan Golongan
 """)
 
-st.markdown(''':red[ajak]''')
-golongan_gendut = [''':red[Pilih Golongan]''',
+
+golongan_gendut = ["Pilih Golongan",
                  "Golongan Ia",
                  "Golongan Ib"]
 masa_kerja = ["Pilih Masa Kerja", 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
                  
 
-golongan_gendutt = st.selectbox(''':red[Masukan Golongan]''',''':red[golongan_gendut]''')
-masa_kerjaa = st.selectbox("Masukan Masa Kerja (Dalam Tahun)",masa_kerja)
+golongan_gendutt = st.selectbox(''':red[Masukan Golongan]''', golongan_gendut)
+masa_kerjaa = st.selectbox(''':red[Masukan Masa Kerja (Dalam Tahun)]''',masa_kerja)
 
 if golongan_gendutt == "Golongan Ia" and 0 <= masa_kerjaa <= 1:
   st.write ("Gaji Pokoknya adalah 1.685.700")
