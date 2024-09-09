6480386619be374baa290389fe6db6a58f1e9713
agent of change <137760740+arispriatna10@users.noreply.github.com>
2024-09-09 11:23:22 +0700

    main1.py

--- main1.py
+++ main1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.success(
+st.success("
 
 st.info("""
 # Aplikasi Cek Gaji Pokok
@@ -59,7 +59,7 @@ elif golongan_gendutt == "Golongan Ia" and 24 <= masa_kerjaa <= 25:
 
 elif golongan_gendutt == "Golongan Ia" and 26 <= masa_kerjaa <= 27:
   st.write ("Gaji Pokoknya adalah 2.522.600")
-)
+")
 
 
 
