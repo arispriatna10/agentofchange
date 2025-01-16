acb9f0345e9ac41dbb5523f6bbe0678c757971de
agent of change <137760740+arispriatna10@users.noreply.github.com>
2025-01-16 11:31:05 +0700

    main.py

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ if asal == "Honorarium Panitia/Pejabat Pengadaan/Penerima Barang dan Jasa PNS Pr
   st.info ("Dokumen Kelengkapan:")
   st.info ("1. Daftar Penerimaan Pembayaran Honor Kuitansi")
   st.info ("2. SK Pejabat/Panitia Pengadaan dan Pejabat/Panitia Penerima Barang dan Jasa")
-  st.info ("3. Rekapitulasi paket pekerjaan yang berisi Informasi tentang nilai dan jenis pekerjaan khusus untuk Pejabat/Panitia pengadaan Dan Pejabat/Panitia Penerima Barang/Jasa, 
-  Kecuali yang 1 (satu) paket pekerjaan melampirkan Fotokopi SPK dan Berita Acara Serah Terima Barang/Jasa khusus untuk pembayaran Honorarium Panitia/Pejabat Penerima Barang/Jasa")
+  st.info ("3. Rekapitulasi paket pekerjaan yang berisi Informasi tentang nilai dan jenis pekerjaan khusus untuk Pejabat/Panitia pengadaan Dan Pejabat/Panitia Penerima Barang/Jasa, Kecuali yang 1 (satu) paket pekerjaan melampirkan Fotokopi SPK dan Berita Acara Serah Terima Barang/Jasa khusus untuk pembayaran Honorarium Panitia/Pejabat Penerima Barang/Jasa")
   st.info ("4. Bukti transfer/SPPT jika pembayarannya melalui transfer")
   st.info ("5. e-Billing dari Bukti Penerimaan Pajak Negara sesuai ketentuan peraturan perundang-undang")
 
