7430a587aec1d3a37658372dc305f58c9a598e94
agent of change <137760740+arispriatna10@users.noreply.github.com>
2025-01-16 11:39:22 +0700

    main.py

--- main.py
+++ main.py
@@ -10,15 +10,7 @@ original_list = ["Pilih Jenis Belanja",
                  "Honorarium Panitia/Pejabat Pengadaan/Penerima Barang dan Jasa PNS Provinsi", 
                  "Honorarium PNS Non Provinsi dan Non PNS (Peserta)",
                  "Upah Harian Lepas",
-                 "Uang Hadiah",
-                 "Perjalanan Dinas Luar Provinsi",
-                 "Perjalanan Dinas Luar Negeri",
-                 "Makanan dan Minuman Rapat",
-                 "Makanan dan Minuman Tamu",
-                 "Honorarium Narasumber",
-                 "Tenaga Ahli Non Sertifikat",
-                 "Makanan dan Minuman Rapat (Platform GRATIS ONGKIR)",
-                 "Makanan dan Minuman Tamu (Platform GRATIS ONGKIR)"]
+                 "Uang Hadiah"]
                  
 
 asal = st.selectbox(''':blue[Masukan Jenis Belanja]''',original_list)
@@ -30,16 +22,28 @@ if asal == "Honorarium Panitia/Pejabat Pengadaan/Penerima Barang dan Jasa PNS Pr
   st.info ("- SK Pejabat/Panitia Pengadaan dan Pejabat/Panitia Penerima Barang dan Jasa")
   st.info ("- Rekapitulasi paket pekerjaan yang berisi Informasi tentang nilai dan jenis pekerjaan khusus untuk Pejabat/Panitia pengadaan Dan Pejabat/Panitia Penerima Barang/Jasa, Kecuali yang 1 (satu) paket pekerjaan melampirkan Fotokopi SPK dan Berita Acara Serah Terima Barang/Jasa khusus untuk pembayaran Honorarium Panitia/Pejabat Penerima Barang/Jasa")
   st.info ("- Bukti transfer/SPPT jika pembayarannya melalui transfer")
-  st.info ("- e-Billing dari Bukti Penerimaan Pajak Negara sesuai ketentuan peraturan perundang-undang")
+  st.info ("- e-Billing dan Bukti Penerimaan Pajak Negara sesuai ketentuan peraturan perundang-undang")
 
 elif asal == "Honorarium PNS Non Provinsi dan Non PNS (Peserta)":
   st.info ("Dokumen Kelengkapan:")
-  st.info ("1. Daftar penerimaan pembayaran uangsaku dan atau transport")
-  st.info ("2. Daftar Hadir")
-  st.info ("3. Berita Acara Penerimaan")
-  st.info ("4. Dokumentasi Kegiatan")
-  st.info ("5. E-Bupot PPh Pasal 22/PPh Pasal 23")
-  st.info ("6. Bukti Setor PPh Pasal 22/PPh Pasal 23")
+  st.info ("- Daftar penerimaan pembayaran uangsaku dan atau transport")
+  st.info ("- Daftar Hadir")
+  st.info ("- Bukti transfer/SPPT jika pembayarannya melalui transfer")
+  st.info ("- e-Billing dan Bukti Penerimaan Pajak Negara sesuai ketentuan peraturan perundang-undang (untuk uang saku)")
+
+elif asal == "Upah Harian Lepas":
+  st.info ("Dokumen Kelengkapan:")
+  st.info ("- Daftar Pembayaran Upah")
+  st.info ("- Daftar Hadir")
+  st.info ("- Bukti transfer/SPPT jika pembayarannya melalui transfer")
+  st.info ("- e-Billing dan Bukti Penerimaan Pajak Negara sesuai ketentuan peraturan perundang-undang")
+
+elif asal == "Uang Hadiah":
+  st.info ("Dokumen Kelengkapan:")
+  st.info ("- Keputusan Gubernur/Keputusan Kepala SKPD tentang Standar besaran Uang/Hadiah dan penetapan pemenang dan atau Keputusan Dewan Hakim/Juri Penilai perlombaan kegiatan")
+  st.info ("- Daftar pembayaran dan atau tanda terima uang hadiah/penghargaan")
+  st.info ("- Bukti Transfer ke pihak penyedia barang/ jasa jika pembayarannya melalui transfer")
+  st.info ("- e-Billing dan Buktti Penerimaan Pajak Negara sesuai ketentuan peraturan perundang-undang")
 
 
 
