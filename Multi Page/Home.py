f2a1616c4d9a859dd2726bf1b70cfc809e45b605
agent of change <137760740+arispriatna10@users.noreply.github.com>
2025-07-19 11:57:43 +0700

    Update Home.py

--- Multi Page/Home.py
+++ Multi Page/Home.py
@@ -10,5 +10,6 @@ st.title("SELAMAT DATANG DI MULTI APP TOOLS")
 st.text("Disini anda bisa mengecek tools bbm dan tools kelengkapan spj")
 st.text("Klik Link berikut untuk mengakses tools cek bbm https://multipagetools.streamlit.app/CEK_BBM")
 st.text("Klik Link berikut untuk mengakses tools cek kelengkapan https://multipagetools.streamlit.app/CEK_KELENGKAPAN_SPJ")
+st.markdown("[Kunjungi Google](https://www.google.com)")
 
 st.sidebar.info("Pilih Halaman di atas")
