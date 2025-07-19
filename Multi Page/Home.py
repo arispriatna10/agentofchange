6bfc0437d66c9efa831cf4ba0a43306400f3dbb8
agent of change <137760740+arispriatna10@users.noreply.github.com>
2025-07-19 12:05:16 +0700

    Update Home.py

--- Multi Page/Home.py
+++ Multi Page/Home.py
@@ -8,9 +8,19 @@ st.set_page_config(
 
 st.title("SELAMAT DATANG DI MULTI APP TOOLS")
 st.text("Disini anda bisa mengecek tools bbm dan tools kelengkapan spj")
-st.text("Klik Link berikut untuk mengakses tools cek bbm https://multipagetools.streamlit.app/CEK_BBM")
-st.text("Klik Link berikut untuk mengakses tools") 
 st.markdown("[CEK BBM](https://multipagetools.streamlit.app/CEK_BBM)")
+st.markdown("""
+    <style>
+    .warna-kustom {
+        background-color: #F9E79F;
+        color: #1D8348;
+        padding: 10px;
+        border-radius: 10px;
+    }
+    </style>
+    <div class="warna-kustom">Ini teks dengan warna latar khusus dan teks hijau</div>
+""", unsafe_allow_html=True)
+
 
 
 st.sidebar.info("Pilih Halaman di atas")
