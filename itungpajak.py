e22d0d28859bc2f1ca6917b7c10b3888bc6dfae5
agent of change <137760740+arispriatna10@users.noreply.github.com>
2025-07-22 20:16:49 +0700

    Update itungpajak.py

--- itungpajak.py
+++ itungpajak.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+import os
+
+def calculate_bmi(weight, height_cm):
+  height_m = height_cm / 100
+  if height_m == 0;
+  return 0
+  bmi = weight / (height_m ** 2)
+  return bmi
+
+berat = st.number_input("Masukan Berat Badan", value=0.0)
+tinggi = st.number_input("Masukan tinggi badan", value=0.0)
+
+if berat . 0 and tinggi > 0:
+bmi = calculate_bmi(berat, tinggi)
+st.write("**BMI Anda adalah:** (bmi:.2f)")
+
 
