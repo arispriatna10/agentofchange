9bbef4323a71f828b22f4f7d8c658204c67a95f7
agent of change <137760740+arispriatna10@users.noreply.github.com>
2025-07-22 20:26:14 +0700

    Update itungpajak.py

--- itungpajak.py
+++ itungpajak.py
@@ -14,6 +14,7 @@ tinggi = st.number_input("Masukan tinggi badan", value=0.0)
 
 if berat == 0 and tinggi > 0:
     bmi = calculate_bmi(berat, tinggi)
-    st.write("**BMI Anda adalah:** (bmi:.2f)")
+  
+st.write("**BMI Anda adalah:** (bmi:.2f)")
 
 
