cd5a5ea9013e2b0f0d370c345bd348fcb19c3eaa
agent of change <137760740+arispriatna10@users.noreply.github.com>
2025-07-25 16:01:05 +0700

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -7,7 +7,7 @@ st.set_page_config(
 
 st.info("""
 # Aplikasi Cek BBM
-Ini adalah aplikasi mengecek kebutuhan BBM Perjalanan Dinas yang mengacu pada Standar Biaya Kebutuhan Bahan Bakar Kendaraan Perjalanan Dinas yang dapat diakses pada link berikut https://drive.google.com/file/d/1bdTsk07F3s_nfRUEd6_uDbTR0fru7Tks/view?usp=sharing
+Ini adalah aplikasi mengecek kebutuhan Bahan Bakar Perjalanan Dinas yang mengacu pada Standar Biaya Kebutuhan Bahan Bakar Kendaraan Perjalanan Dinas yang dapat diakses pada link berikut https://drive.google.com/file/d/1bdTsk07F3s_nfRUEd6_uDbTR0fru7Tks/view?usp=sharing
 """)
 
 
